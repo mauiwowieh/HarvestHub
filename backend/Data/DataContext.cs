4f8c1826b6afc11843db84cb5dd74eb5bb15b04c
mauiwowieh <senaidaduran387@gmail.com>
2024-12-01 21:23:48 +0100

    PBI 3: Initial set up

--- backend/Data/DataContext.cs
+++ backend/Data/DataContext.cs
@@ -1,8 +1,10 @@
 using System;
+using backend.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace backend.Data;
 
 public class DataContext(DbContextOptions options) : DbContext(options)
 {
+    public DbSet<AppUser> Users { get; set; }
 }
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
   title = 'HarvestHub';
+  users: any;
+
+  ngOnInit() {
+    this.http.get('http://localhost:5000/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.log("Error has ocurred"),
+      complete: () => { }
+    })
+  }
 }
